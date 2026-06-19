1556bd209134b1360bb29a640455a9a0a64531d8
hushus37182 <assasever@mail.ru>
2026-06-19 19:45:23 +0700

    initial

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# hyrpland-winwrap
-run live wallpapers without memory leaks! 
+## hyprland-winwrap
+### Display any window as a background in Hyprland
+fixed the bug with freezes on workspace swaps and window killing that hyprwinwrap has
+---
+this plugin renders window textures on background, which can be used to bypass live wallpapers memory leaks (e.g. run mpv on bg) and it's physically unable to kill a window by itself.
+
+tested on hyprland 0.54.3
+
+#### Installation
+```bash
+hyprpm add https://github.com/hushus37182/hyprland-winwrap.git
+hyprpm enable hyprland-winwrap
+```
+
+to run windows on background, add following to your hyprland.conf:
+   ```
+    plugin{
+        hyprland-winwrap{
+            title=your_window_title
+                # or
+            class=your_window_class
+        }
+    }
+   ```
+
+the window also must be opened somewhere 
+it could be a special workspace or some workspace 99 
---
+++ main.cpp
@@ -0,0 +1,148 @@
+#include <hyprland/src/plugins/PluginAPI.hpp>
+#include <hyprland/src/event/EventBus.hpp> 
+#include <hyprland/src/Compositor.hpp>
+#include <hyprland/src/render/Renderer.hpp> 
+#include <hyprland/src/render/pass/PassElement.hpp>
+#include <hyprland/src/render/OpenGL.hpp>
+#include <hyprland/src/desktop/state/FocusState.hpp> 
+#include <hyprland/src/desktop/view/Window.hpp>
+#include <hyprland/src/protocols/XWaylandShell.hpp>
+#include <hyprland/src/protocols/XDGShell.hpp>  
+#include <hyprland/src/xwayland/XSurface.hpp>  
+#include <hyprland/src/desktop/DesktopTypes.hpp> 
+#include <thread>
+#include <chrono>
+#include <unistd.h>
+#include <string>
+#include <cstdlib>
+#include <filesystem>
+
+using namespace std;
+inline HANDLE PHANDLE = nullptr;
+
+int ct = 0;
+int ct2;
+PHLWORKSPACE workspace;
+
+class CProxyVideoElement : public IPassElement {
+public:
+    struct SProxyData {
+        Hyprutils::Math::CBox logicalBox;
+        PHLWINDOWREF pWindowRef;
+    };
+
+    CProxyVideoElement(SProxyData data_) : data(data_) {}
+    virtual ~CProxyVideoElement() override = default;
+
+    virtual void draw(const CRegion& damage) override {
+        try {
+            auto pWindow = data.pWindowRef.lock();
+            if (!pWindow) {
+                return; 
+            }
+
+            auto pWlSurface = pWindow->wlSurface();
+            if (!pWlSurface || !pWlSurface->resource()) {
+                return;
+            }
+
+            auto pWindowTexture = pWlSurface->resource()->m_current.texture;
+            if (pWindowTexture == nullptr) {
+                auto pMonitor = Desktop::focusState()->monitor();
+                g_pHyprRenderer->damageMonitor(pMonitor);
+                return;
+            }
+            auto pMonitor = Desktop::focusState()->monitor();
+            if (workspace != pMonitor->m_activeWorkspace && !ct2){
+                thread([]() {
+                    auto path = filesystem::path(__FILE__).parent_path() / "build/plugin.so";
+                    auto cmd = string("hyprctl plugin unload ") + path.c_str();
+                    system(cmd.c_str());
+                }).detach();
+                ct2=1;
+            }
+
+            CHyprOpenGLImpl::STextureRenderData texRenderData;
+            texRenderData.a = 1.0f;
+            g_pHyprOpenGL->renderTexture(pWindowTexture, data.logicalBox, texRenderData);
+
+        } catch (...) {
+            HyprlandAPI::addNotification(PHANDLE, "Hyprland-winwrap: Fatal Error", CHyprColor{1.0, 0.0, 0.0, 1.0}, 5000);
+        }
+    }
+
+
+    virtual bool needsLiveBlur() override { return false; }
+    virtual bool needsPrecomputeBlur() override { return false; }
+    virtual const char* passName() override { return "WallpaperElement"; }
+
+private:
+    SProxyData data;
+};
+
+APICALL EXPORT std::string PLUGIN_API_VERSION() {
+    return HYPRLAND_API_VERSION;
+}
+
+APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
+    PHANDLE = handle;
+
+    const std::string COMPOSITOR_HASH = __hyprland_api_get_hash();
+    const std::string CLIENT_HASH = __hyprland_api_get_client_hash();
+    
+    if (COMPOSITOR_HASH != CLIENT_HASH) {
+        throw std::runtime_error(CLIENT_HASH);
+    }
+
+    HyprlandAPI::addConfigValue(PHANDLE, "plugin:hyprland-winwrap:class", Hyprlang::STRING{"kitty-bg"});
+    HyprlandAPI::addConfigValue(PHANDLE, "plugin:hyprland-winwrap:title", Hyprlang::STRING{""});
+
+    HyprlandAPI::reloadConfig();
+
+    static auto renderHook = Event::bus()->m_events.render.stage.listen([=](eRenderStage stage) {
+        try {
+            if (stage != eRenderStage::RENDER_POST_WALLPAPER) return;
+
+            auto pMonitor = Desktop::focusState()->monitor();
+            if (!pMonitor) return;
+
+            PHLWINDOW pWindow;
+            static auto* const PCLASS = (Hyprlang::STRING const*)HyprlandAPI::getConfigValue(PHANDLE, "plugin:hyprland-winwrap:class")->getDataStaticPtr();
+            static auto* const PTITLE = (Hyprlang::STRING const*)HyprlandAPI::getConfigValue(PHANDLE, "plugin:hyprland-winwrap:title")->getDataStaticPtr();
+
+            string pclass = *PCLASS;
+            string ptitle = *PTITLE;
+            for (auto const& w : g_pCompositor->m_windows) {
+                if (w->m_title==ptitle || w->m_class==pclass){ pWindow=w;}
+            }
+            if (pWindow == nullptr){ 
+                if (ct==0){
+                    HyprlandAPI::addNotification(PHANDLE, "no bg windows found",CHyprColor{1.0, 1.0, 0.2, 1.0}, 2500); 
+                    ct++;
+                }
+                return; 
+            }
+            else{ct=0;}
+
+            if (!workspace){workspace = pMonitor->m_activeWorkspace;}
+
+            CProxyVideoElement::SProxyData proxyData;
+            proxyData.logicalBox = pMonitor->logicalBox();
+            proxyData.pWindowRef = pWindow;
+            g_pHyprRenderer->m_renderPass.add(makeUnique<CProxyVideoElement>(proxyData));
+
+        } catch (const exception& e) {
+            string errMsg = "Hyprland-winwrap: Fatal Error: ";
+            errMsg += e.what();
+            HyprlandAPI::addNotification(PHANDLE, errMsg, CHyprColor{1.0, 0.0, 0.0, 1.0}, 5000);
+        } catch (...) {
+            HyprlandAPI::addNotification(PHANDLE, "Hyprland-winwrap: Fatal Error", CHyprColor{1.0, 0.0, 0.0, 1.0}, 5000);
+        }
+    });
+
+    return {"Hyprland-winwrap", "proxy wallpaper", "hus", "1.0"};
+}
+
+APICALL EXPORT void PLUGIN_EXIT() {
+    //...
+}
